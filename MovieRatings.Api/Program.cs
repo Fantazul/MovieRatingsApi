29bd5613e670b3513ef615d4b195d02995c75fc0
Thanos Dimitriades <fantazul@proton.me>
2025-01-08 19:45:05 +0200

    feat: add movie model and repository

--- MovieRatings.Api/Program.cs
+++ MovieRatings.Api/Program.cs
@@ -1,11 +1,14 @@
+using MovieRatings.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container .
-
+// Add services to the container
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
---
+++ MovieRatings.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieRatings.Application.Models;
+
+namespace MovieRatings.Application.Repositories;
+
+public interface IMovieRepository
+{
+    Task<Movie?> GetByIdAsync(Guid id);
+    
+    Task<bool> CreateAsync(Movie movie);
+
+    Task<IEnumerable<Movie>> GetAllAsync();
+
+    Task<bool> UpdateAsync(Movie movie);
+
+    Task<bool> DeleteByIdAsync(Guid id);
+}
